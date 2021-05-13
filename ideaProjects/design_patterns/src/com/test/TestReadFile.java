d6261ea3682fdbdf1aada6def33f9d2fd10f7562
HUER <2871423731@qq.com>
2021-05-13 09:08:49 +0800

    设计模式学习

---
+++ ideaProjects/design_patterns/src/com/test/TestReadFile.java
@@ -0,0 +1,49 @@
+package com.test;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * @Classname : Test1
+ * @Description : TODO
+ * @Created by kingdee
+ * @Date : 2021/5/11
+ */
+public class TestReadFile {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null; //用于包装InputStreamReader,提高处理性能。因为BufferedReader有缓冲的，而InputStreamReader没有。
+        try {
+            String str = "";
+            String str1 = "";
+            fis = new FileInputStream("C:\\Users\\kingdee\\Desktop\\abc.txt");// FileInputStream
+            // 从文件系统中的某个文件中获取字节
+            isr = new InputStreamReader(fis);// InputStreamReader 是字节流通向字符流的桥梁,
+            br = new BufferedReader(isr);// 从字符输入流中读取文件中的内容,封装了一个new InputStreamReader的对象
+            while ((str = br.readLine()) != null) {
+                if (!" ".equals(String.valueOf(str.charAt(0)))) {
+                    str1 += " " + str;
+                } else {
+                    str1 += "\n"  + str;
+                }
+            }
+            // 当读取的一行不为空时,把读到的str的值赋给str1
+            System.out.println(str1);// 打印出str1
+        } catch (FileNotFoundException e) {
+            System.out.println("找不到指定文件");
+        } catch (IOException e) {
+            System.out.println("读取文件失败");
+        } finally {
+            try {
+                br.close();
+                isr.close();
+                fis.close();
+                // 关闭的时候最好按照先后顺序关闭最后开的先关闭所以先关s,再关n,最后关m
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
